7838866ab363688ed8126da89f11605848ab2186
Jacob Ho <clh161@gmail.com>
2020-08-15 19:22:41 +0100

    skills

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -2,21 +2,76 @@
 
 type Role = 'TANKER' | 'WARRIOR'
 type Element = 'WATER' | 'LIGHT'
-
+type Resistance = { type: 'BASIC' | 'DARK' | 'FIRE' | 'EARTH', value: number }
+type Attribute = 'HP' | 'RANGE_ATK' | 'LIGHT_ATK' | 'WEAPON_SKILL_REGEN_SPEED'
+type SkillTarget = 'SELF' | 'PARTY'
+type Skill = {
+    target: SkillTarget,
+    attribute: Attribute,
+    value: number,
+}
 type Hero = {
-    name: string, role: Role, element: Element
+    name: string,
+    role: Role,
+    element: Element,
+    atk: number,
+    criticalHitChance: number,
+    hp: number,
+    def: number,
+    damageReduction: number,
+    cardSlot: number,
+    resistance: Array<Resistance>,
+    skills: Array<Skill>,
 }
 
 const heroes: Array<Hero> = [
     {
-        "name": "Marina",
-        "role": "TANKER",
-        "element": "WATER"
+        name: 'Marina',
+        role: 'TANKER',
+        element: 'WATER',
+        atk: 659,
+        criticalHitChance: 0.0,
+        hp: 23471,
+        def: 398,
+        damageReduction: 19,
+        cardSlot: 2,
+        resistance: [
+            {type: 'FIRE', value: 0.3},
+            {type: 'EARTH', value: -0.3}
+        ],
+        skills: [{
+            target: 'PARTY',
+            attribute: 'HP',
+            value: 0.4
+        }, {
+            target: 'SELF',
+            attribute: 'WEAPON_SKILL_REGEN_SPEED',
+            value: 0.08
+        }]
     },
     {
-        "name": "Lapice",
-        "role": "WARRIOR",
-        "element": "LIGHT"
+        name: 'Lapice',
+        role: 'WARRIOR',
+        element: 'LIGHT',
+        atk: 629,
+        criticalHitChance: 0.08,
+        hp: 19941,
+        def: 125,
+        damageReduction: 26,
+        cardSlot: 2,
+        resistance: [
+            {type: 'BASIC', value: -0.3},
+            {type: 'DARK', value: 0.3},
+        ],
+        skills: [{
+            target: 'PARTY',
+            attribute: 'LIGHT_ATK',
+            value: 0.4
+        }, {
+            target: 'SELF',
+            attribute: 'WEAPON_SKILL_REGEN_SPEED',
+            value: 0.06
+        }]
     }
 ]
 
--- src/pages/heroes.js
+++ src/pages/heroes.js
@@ -4,23 +4,65 @@ import React from 'react';
 
 import {Table} from 'antd';
 
-import  data from '../data/heroes.js'
+import data from '../data/heroes.js'
+
 const columns = [
     {
         title: 'Name',
         dataIndex: 'name',
         key: 'name'
-    },
-    {
+    }, {
         title: 'Role',
         dataIndex: 'role',
         key: 'role',
-    },
-    {
+    }, {
         title: 'Element',
         dataIndex: 'element',
         key: 'element',
-    }
+    }, {
+        title: 'Attack',
+        dataIndex: 'atk',
+        key: 'atk',
+    }, {
+        title: 'HP',
+        dataIndex: 'hp',
+        key: 'hp',
+    }, {
+        title: 'Defence',
+        dataIndex: 'def',
+        key: 'def',
+    }, {
+        title: 'Damage Reduction',
+        dataIndex: 'damageReduction',
+        key: 'damageReduction',
+    }, {
+        title: 'Critical Hit Chance',
+        dataIndex: 'criticalHitChance',
+        key: 'criticalHitChance',
+    }, {
+        title: 'Resistance',
+        dataIndex: 'resistance',
+        key: 'resistance',
+        render: resistance => {
+            return resistance.map(res => {
+                return <div>{`${res.type}: ${res.value * 100}%`}</div>
+            })
+        }
+    }, {
+        title: 'Card Slot',
+        dataIndex: 'cardSlot',
+        key: 'cardSlot',
+    }, {
+        title: 'Skills',
+        dataIndex: 'skills',
+        key: 'skills',
+        render: skills => {
+            return skills.map(skill => {
+                const target = skill.target === 'SELF' ? '' :`[${skill.target}]`
+                return <div>{`${target}${skill.attribute}: ${skill.value * 100}%`}</div>
+            })
+        }
+    },
 ];
 
 
