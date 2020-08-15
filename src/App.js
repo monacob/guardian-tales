81358e410d5765ec05e838b903bca8e1bbcf146c
Jacob Ho <clh161@gmail.com>
2020-08-15 20:17:59 +0100

    router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,71 @@ import React from 'react';
 import './App.css';
 import Home from './pages/home'
 import 'antd/dist/antd.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Heroes from "./pages/heroes";
+import {UserOutlined, LaptopOutlined, NotificationOutlined} from '@ant-design/icons';
+import {Layout, Menu, Breadcrumb} from 'antd';
+import {createBrowserHistory} from 'history';
+
+const {SubMenu} = Menu;
+const {Header, Content, Sider} = Layout;
+
+const history = createBrowserHistory();
 
 function App() {
-  return (
-    <div className="App">
-      <Home/>
+    return (
+        <Router history={history}>
+            <div className="App">
+                <Layout>
+                    <Header className="header">
+                        <div className="logo"/>
+                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                            <Menu.Item onClick={() => history.push('/')} key="1">Home</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Layout>
+                        <Sider width={200} className="site-layout-background">
+                            <Menu
+                                style={{height: '100%', borderRight: 0}}
+                            >
+                                <SubMenu onTitleClick={() => {
+                                    history.push('/heroes')
+                                }}
+                                         key="sub1"
+                                         icon={<UserOutlined/>}
+                                         title="Heroes">
 
-    </div>
-  );
+                                </SubMenu>
+                            </Menu>
+                        </Sider>
+                        <Layout style={{padding: '0 24px 24px'}}>
+                            <Content
+                                className="site-layout-background"
+                                style={{
+                                    padding: 24,
+                                    margin: 0,
+                                    minHeight: 280,
+                                }}
+                            >
+                                <Switch>
+                                    <Route path="/heroes">
+                                        <Heroes/>
+                                    </Route>
+                                    <Route path="/">
+                                        <Home/>
+                                    </Route>
+                                </Switch>
+                            </Content>
+                        </Layout>
+                    </Layout>
+                </Layout>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
